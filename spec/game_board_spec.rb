337ff3abc05af7a8192ca4722523f87e1f9e6a4c
bright okike <brightokike@gmail.com>
2019-06-14 19:49:33 +0300

    game board test

--- spec/game_board_spec.rb
+++ spec/game_board_spec.rb
@@ -33,11 +33,37 @@ describe GameBoard do
             before do
                 allow(player).to receive(:player_action).and_return([0,0])   
                 allow(player).to receive(:mark).and_return("X")
-                board.instance_variable_set(:@board,[[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]] ) 
-                
+                board.instance_variable_set(:@board,[[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]] )       
             end
             it 'the position is' do
                 expect(board.position_taken(player.player_action)).to eql(nil)
+            end
+        end
+    end
+
+    describe "#move" do
+        context 'the position is free or not' do
+            before do
+                allow(player).to receive(:player_action).and_return([0,2])   
+                allow(player).to receive(:mark).and_return("X")
+                board.instance_variable_set(:@board,[[nil,nil,'X'],[nil,nil,nil],[nil,nil,nil]] )       
+            end
+            it 'returns fail is the position is not equal to nil' do
+                expect(board.move(player.player_action, player.mark)).to eql('fail')
+            end
+        end
+    end
+
+    # test for nil
+    describe "#move" do
+        context 'the position is free or not' do
+            before do
+                allow(player).to receive(:player_action).and_return([0,1])   
+                allow(player).to receive(:mark).and_return("X")
+                board.instance_variable_set(:@board,[[nil,nil,'X'],[nil,nil,nil],[nil,nil,nil]] )       
+            end
+            it 'returns nil is the position is  equal to nil' do
+                expect(board.move(player.player_action, player.mark)).to eql(nil)
             end
         end
     end
